17a50926a33b0063f171e75d9459797d4ba80e8a
Jack Moran <jvckmorvn@gmail.com>
2023-10-30 17:50:28 +0100

    formik

--- app/App.js
+++ app/App.js
@@ -1,37 +1,13 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Screen from './components/Screen';
 import { useState } from 'react';
-import AppPicker from './components/AppPicker';
-
-const categories =[
-  {
-    label: 'Furniture',
-    value: 1
-  },
-  {
-    label: 'Clothing',
-    value: 2
-  },
-  {
-    label: 'Electronics',
-    value: 3
-  }
-];
+import LoginScreen from './screens/LoginScreen';
 
 export default function App() {
   const [category, setCategory] = useState(categories[0]);
 
   return (
     <GestureHandlerRootView>
-      <Screen>
-        <AppPicker
-          selectedItem={category}
-          onSelectItem={item => setCategory(item)}
-          items={categories}
-          icon='apps'
-          placeholder='Category'
-        />
-      </Screen>
+      <LoginScreen/>
     </GestureHandlerRootView>
   );
 }
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,57 @@
+import { Image, StyleSheet } from "react-native";
+import { Formik } from 'formik';
+import Screen from "../components/Screen";
+import AppTextInput from "../components/AppTextInput";
+import AppButton from "../components/AppButton";
+
+export default function LoginScreen() {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/logo-red.png')}/>
+      <Formik
+        initialValues={{email: '', password: ''}}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({handleChange, handleSubmit}) => (
+          <>
+          <AppTextInput
+            autoCapitalize='none'
+            autoCorrect={false}
+            icon='email'
+            keyboardType='email-address'
+            onChangeText={handleChange('email')}
+            placeholder='Email address'
+            textContentType='emailAddress'
+          />
+          <AppTextInput
+            autoCapitalize='none'
+            autoCorrect={false}
+            icon='key'
+            onChangeText={handleChange('password')}
+            placeholder='Password'
+            secureTextEntry
+            textContentType='password'
+          />
+          <AppButton 
+            title='Login'
+            onPress={handleSubmit}
+          />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  ); 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
+    marginTop: 50,
+    marginBottom: 20
+  }
+});
